29133b73429ffd7b5b8e58a8931c8d24614a1164
zhangb96 <bokhaam.zhang@mail.utoronto.ca>
2020-05-21 23:10:05 -0400

    GUI optimization
    Added restriction to pve game creation when overlines rule is not appropreate

--- app/src/main/java/com/trashsoftware/wuziqi/MainActivity.java
+++ app/src/main/java/com/trashsoftware/wuziqi/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -17,19 +19,23 @@ public class MainActivity extends AppCompatActivity {
     final static String AI_FIRST_KEY = "aiFirst";
     final static String DIFFICULTY_KEY = "difficulty";
 
-    private Spinner overlinesSpinner;
+    private Spinner overlinesSpinner;  // 长连选项
     private Switch aiFirstSwitch;
     private SeekBar difficultyBar;
+    private Button pveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        overlinesSpinner = findViewById(R.id.overlinesSpinner);
-        overlinesSpinner.setSelection(RulesSet.OVERLINES_WINNING);
         aiFirstSwitch = findViewById(R.id.aiFirstSwitch);
         difficultyBar = findViewById(R.id.difficultyBar);
+        pveButton = findViewById(R.id.pveButton);
+
+        overlinesSpinner = findViewById(R.id.overlinesSpinner);
+        overlinesSpinner.setSelection(RulesSet.OVERLINES_WINNING);
+        overlinesSpinner.setOnItemSelectedListener(new OverlinesSelectionListener());
     }
 
     public void onPveButtonClicked(View view) {
@@ -59,5 +65,21 @@ public class MainActivity extends AppCompatActivity {
 
     private int getDifficultyLevel() {
         return difficultyBar.getProgress();
+    }
+
+    private class OverlinesSelectionListener implements AdapterView.OnItemSelectedListener {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if (position == RulesSet.OVERLINES_WINNING) {
+                pveButton.setEnabled(true);
+            } else {
+                pveButton.setEnabled(false);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
     }
 }
